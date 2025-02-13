85b54ab6bacb5c12552471d10ba328d07bdd6414
akrm al-hakimi <alhakimiakrmj@gmail.com>
2025-02-12 23:19:11 -0500

    batman

---
+++ backend/test/basic.test.js
@@ -0,0 +1,5 @@
+describe('Basic Backend Test', () => {
+  test('Basic test passes', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ frontend/tests/basic.test.js
@@ -0,0 +1,5 @@
+describe('Basic Test Suite', () => {
+  test('Initial test passes', () => {
+    expect(true).toBe(true);
+  });
+});
